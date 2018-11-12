fe23f4dfe75641ecaeef2b61822234fb99fc6976
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-12 19:06:10 +0000

    Style tweaks for <Header />

--- frontend/components/Page/styles.js
+++ frontend/components/Page/styles.js
@@ -1,14 +1,17 @@
 import styled, { injectGlobal } from "styled-components";
 
+const themeDark = {
+  colour: "white",
+  colourSecondary: "#f97627",
+  colourTertiary: "#3aa1ec",
+  backgroundColour: "#525252"
+};
+
 const theme = {
   colour: "black",
   colourSecondary: "#f97627",
   colourTertiary: "#3aa1ec",
   backgroundColour: "white",
-  // colour: "white",
-  // colourSecondary: "#f97627",
-  // colourTertiary: "#3aa1ec",
-  // backgroundColour: "#525252",
   maxWidth: "1000px",
   bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 };
