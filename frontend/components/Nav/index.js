8143a2e369fbdd1b3e46f932052433fdf6f579d8
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-14 19:24:19 +0000

    Can signup from the frontend

--- frontend/components/Nav/index.js
+++ frontend/components/Nav/index.js
@@ -4,8 +4,12 @@ import NavStyled from "./styles";
 
 const Nav = () => (
   <NavStyled>
-    <Link href="/">
-      <a>Home!</a>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    
+    <Link href="/login">
+      <a>Login</a>
     </Link>
   </NavStyled>
 );
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 const Home = () => (
   <div>
     <h1>Home page</h1>
+    <p>Challenges will go here</p>
   </div>
 );
 
