5cb7775f461d058ea7f4153c974cc9433edc9c82
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-22 20:36:10 +0000

    Add e2e test for 3rd user not seeing 1st vs 2nd challenge

--- frontend/cypress/integration/homepage.js
+++ frontend/cypress/integration/homepage.js
@@ -15,7 +15,7 @@ describe("Given I visit the Homepage", () => {
     });
   });
 
-  describe("When signing in", () => {
+  describe("When signed in as Bob", () => {
     before(() => {
       cy.get('[href="/signin"]').click();
       cy.contains("Log in to your account");
@@ -40,6 +40,39 @@ describe("Given I visit the Homepage", () => {
 
     it("Should let me click on a teaser and see the full challenge", () => {
       cy.get(":nth-child(1) > a > h3").click();
+      expect(true).to.eq(true);
+    });
+
+    after(() => {
+      cy.get("button").click();
+    });
+  });
+
+  describe("When signed in as Chuck", () => {
+    before(() => {
+      cy.get('[href="/signin"]').click();
+      cy.contains("Log in to your account");
+
+      cy.get("button").contains("Log in");
+
+      cy.get(':nth-child(1) > fieldset > [for="email"] > input')
+        .type("c@c.com")
+        .should("have.value", "c@c.com");
+
+      cy.get('[for="password"] > input').type("asdasd");
+
+      cy.get(":nth-child(1) > fieldset > button", { force: true }).click();
+    });
+
+    it("Should render correctly", () => {
+      cy.contains("Hi, Chuck E. Cheese");
+      cy.contains("Challenge a friend");
+      cy.contains("Sign out");
+      cy.get(":nth-child(1) > a");
+    });
+
+    it("Should not show me Bob and Sue's challenge teaser", () => {
+      cy.get(":nth-child(1) > a > h3").should("not.exist");
       expect(true).to.eq(true);
     });
   });
